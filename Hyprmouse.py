5129929485c9f3a4bf74f8c6587ccf214a3f318f
nnra6864 <enenragd@gmail.com>
2024-03-08 07:19:28 +0100

    New init

--- Hyprmouse.py
+++ Hyprmouse.py
@@ -1,228 +1,136 @@
-import setproctitle, cairo
-import subprocess, re, gi
+import setproctitle, subprocess, cairo, os, re, gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('GtkLayerShell', '0.1')
-from gi.repository import Gtk, Gdk, GtkLayerShell
+from gi.repository import Gtk, Gdk, GtkLayerShell, GLib
+
+
+"""
+UTILS
+"""
+
+
+def hex_to_rgb(hex_string):
+    return tuple(int(hex_string[i:i+2], 16) / 255 for i in (1, 3, 5))
 
 
 def get_screen_res():
     output = subprocess.check_output(["hyprctl", "monitors", "all"]).decode("utf-8")
-    match = re.search(r"\d+x\d+", output)
-    if match:
-        wh = match.group()
-        w, h = map(int, wh.split('x'))
-        return w, h
-    else:
-        print("Failed to retrieve the resolution.")
-width, height = get_screen_res()
-step_size = 500
+    wh = re.search(r"\d+x\d+", output).group()
+    return map(int, wh.split('x'))
+
+
+"""
+FUNCTIONS
+"""
+
+
+def load_config():
+    global config
+    cfg_path = os.path.expanduser("~/.config/hypr/hyprmouse.conf")
+    try:
+        with open(cfg_path, 'r') as cfg:
+            for line in cfg:
+                key, value = line.strip().split('=')
+                key = key.strip()
+                value = value.strip()
+                if key in config:
+                    if value == "True":
+                        config[key] = True
+                    elif value == "False":
+                        config[key] = False
+                    elif value.isdigit():
+                        config[key] = int(value)
+                    else:
+                        config[key] = value
+    except FileNotFoundError:
+        print("Config file not found.")
+    except Exception as e:
+        print("An error occurred while loading the config file:", e)
+
 
 def draw_grid(window, cr):
-    global step_size
-    print("here")
-    cr.set_source_rgb(0.9255, 0.9373, 0.9569)
-    cr.set_line_width(0.5)
+    global config, width, height
+    if not config["show_grid"]: return
+    cr.set_source_rgb(*hex_to_rgb(config["grid_color"]))
+    cr.set_line_width(config["grid_thickness"])
 
-    for x in range((width // 2) % step_size, width, step_size):
+    for x in range((width // 2) % config["spacing"], width, config["spacing"]):
         cr.move_to(x, 0)
         cr.line_to(x, height)
         cr.stroke()
 
-    for y in range((height // 2) % step_size, height, step_size):
+    for y in range((height // 2) % config["spacing"], height, config["spacing"]):
         cr.move_to(0, y)
         cr.line_to(width, y)
         cr.stroke()
 
-    cr.set_source_rgb(0.1328, 0.1569, 0.1569)
-    cr.select_font_face("Cascadia Code", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    cr.set_font_size(20)
-    
-    for x_offset in range(-2, 3):
-        for y_offset in range(-2, 3):
-            if x_offset != 0 or y_offset != 0:
-                for x in range((width // 2) % step_size, width, step_size):
-                    for y in range((height // 2) % step_size, height, step_size):
-                        label = f"{x}, {height - y}"
-                        x_text_offset = x - cr.text_extents(label)[4] // 2 + x_offset
-                        y_text_offset = y + cr.text_extents(label)[3] // 2 + y_offset
-                        cr.move_to(x_text_offset, y_text_offset - cr.text_extents(label)[3])
-                        cr.show_text(label)
+    #Try to remove this func from the draw call later if I figure out a way to not erase the grid whilst doing it :/
 
-                        relative_label = f"{x - prevXPos}, {height - (prevYPos + y)}"
-                        x_text_offset = x - cr.text_extents(relative_label)[4] // 2 + x_offset
-                        y_text_offset = y + cr.text_extents(relative_label)[3] // 2 + y_offset
-                        cr.move_to(x_text_offset, y_text_offset + cr.text_extents(relative_label)[3])
-                        cr.show_text(relative_label)
+
+def draw_numbers(window, cr):
+    global config, width, height, posX, posY
 
     
-    for x in range((width // 2) % step_size, width, step_size):
-        for y in range((height // 2) % step_size, height, step_size):
-            cr.set_source_rgb(0.5333, 0.7529, 0.8157)
-            label = f"{x}, {height - y}"
-            x_text_offset = x - cr.text_extents(label)[4] // 2
-            y_text_offset = y + cr.text_extents(label)[3] // 2
-            cr.move_to(x_text_offset, y_text_offset - cr.text_extents(label)[3])
-            cr.show_text(label)
 
-            cr.set_source_rgb(0.9216, 0.7961, 0.5455)
-            relative_label = f"{x - prevXPos}, {height - (prevYPos + y)}"
-            x_text_offset = x - cr.text_extents(relative_label)[4] // 2
-            y_text_offset = y + cr.text_extents(relative_label)[3] // 2
-            cr.move_to(x_text_offset, y_text_offset + cr.text_extents(relative_label)[3])
-            cr.show_text(relative_label)
+def update(window):
+    window.queue_draw()
+    return True
 
-            cr.set_source_rgb(0.9255, 0.9373, 0.9569)
-            cr.arc(x, y, 3, 0, 2 * 3.14159)
-            cr.fill_preserve()
-
-    return False
-
-
-xPos = 0
-yPos = 0
-prevXPos = width // 2
-prevYPos = height // 2
-direction = 1
-liveUpdate = True
-
-def editAxis(axis, window, event):
-    global liveUpdate
-    global isEditingX, isEditingY
-    global xPos, yPos, prevXPos, prevYPos
-    
-    if isEditingX: yPos = prevYPos
-    else: xPos = prevXPos
-
-    if event.keyval == Gdk.KEY_Escape:
-        isEditingX = isEditingY = False
-        subprocess.run(["ydotool", "mousemove", "-a", f"{prevXPos}", f"{height - prevYPos}"])
-        xPos = yPos = 0
-        return
-    if event.keyval == Gdk.KEY_Return:
-        subprocess.run(["ydotool", "mousemove", "-a", f"{xPos}", f"{height - yPos}"])
-        isEditingX = isEditingY = False
-        prevXPos = xPos
-        prevYPos = yPos
-        xPos = yPos = 0
-        window.queue_draw()
-        return
-
-    if chr(event.keyval).isdigit():
-        if axis == 'x': xPos = xPos * 10 + int(chr(event.keyval))
-        else: yPos = yPos * 10 + int(chr(event.keyval))
-        if liveUpdate: subprocess.run(["ydotool", "mousemove", "-a", f"{xPos}", f"{height - yPos}"])
-    elif event.keyval == Gdk.KEY_BackSpace:
-        if axis == 'x': xPos = xPos // 10
-        else: yPos = yPos // 10
-        if liveUpdate: subprocess.run(["ydotool", "mousemove", "-a", f"{xPos}", f"{height - yPos}"])
-
-
-def editAxisRelative(axis, window, event):
-    global isRelEditingX, isRelEditingY, direction
-    global xPos, yPos, prevXPos, prevYPos
-
-    if event.keyval == Gdk.KEY_Escape:
-        isRelEditingX = isRelEditingY = False
-        subprocess.run(["ydotool", "mousemove", "-a", f"{prevXPos}", f"{height - prevYPos}"])
-        direction = 1
-        return
-    if event.keyval == Gdk.KEY_Return:
-        subprocess.run(["ydotool", "mousemove", "-a", f"{prevXPos + (xPos * direction)}", f"{height - prevYPos - (yPos * direction)}"])
-        isRelEditingX = isRelEditingY = False
-        prevXPos = prevXPos + (xPos * direction)
-        prevYPos = height - prevYPos + (yPos * direction)
-        xPos = yPos = 0
-        direction = 1
-        window.queue_draw()
-        return
-
-    if chr(event.keyval) == '+' or chr(event.keyval) == '=':
-        direction = 1
-    if chr(event.keyval) == '-' or chr(event.keyval) == '_':
-        direction = -1
-    if chr(event.keyval).lower == 'i':
-        direction = -direction
-
-    if chr(event.keyval).isdigit():
-        if axis == 'x': xPos = xPos * 10 + int(chr(event.keyval))
-        else: yPos = yPos * 10 + int(chr(event.keyval))
-    elif event.keyval == Gdk.KEY_BackSpace:
-        if axis == 'x': xPos = xPos // 10
-        else: yPos = yPos // 10
-    
-    if liveUpdate: subprocess.run(["ydotool", "mousemove", "-a", f"{prevXPos + (xPos * direction)}", f"{height - prevYPos - (yPos * direction)}"])
-
-isEditingX = False
-isEditingY = False
-isRelEditingX = False
-isRelEditingY = False
 
 def on_key_press(window, event):
     if event.keyval == Gdk.KEY_Delete:
         Gtk.main_quit()
 
-    global step_size
-    global liveUpdate
-    global isEditingX, isEditingY, isRelEditingX, isRelEditingY
-    global xPos, yPos, prevXPos, prevYPos
-
-    if isEditingX:
-        editAxis('x', window, event)
-        return
-    if isEditingY:
-        editAxis('y', window, event)
-        return
-    if isRelEditingX:
-        editAxisRelative('x', window, event)
-        return
-    if isRelEditingY:
-        editAxisRelative('y', window, event)
-        return
-
-    if event.keyval == Gdk.KEY_Escape or event.keyval == Gdk.KEY_Return:
-        Gtk.main_quit()
-
-    if chr(event.keyval).lower() == 'r':
-        prevXPos = width // 2
-        prevYPos = height // 2
-        subprocess.run(["ydotool", "mousemove", "-a", f"{prevXPos}", f"{prevYPos}"])
-        window.queue_draw()
-
-    if chr(event.keyval).lower() == 'l':
-        liveUpdate = not liveUpdate
-    
-    if chr(event.keyval).lower() == '+' or chr(event.keyval).lower() == '=':
-        step_size = step_size - 100
-        window.queue_draw()
-
-    if chr(event.keyval).lower() == '-' or chr(event.keyval).lower() == '_':
-        step_size = step_size + 100
-        window.queue_draw()
-
-    isEditingX = chr(event.keyval) == 'X';
-    isEditingY = chr(event.keyval) == 'Y';
-    isRelEditingX = chr(event.keyval) == 'x';
-    isRelEditingY = chr(event.keyval) == 'y';
-
 
 def draw_window():
+    global config
     window = Gtk.Window()
     window.set_title("Hyprmouse")
     window.set_size_request(width, height)
-    window.resize(1, 1)
-    window.set_app_paintable(True)
-    
+    window.resize(1, 1) #No clue why this is needed but it triggers the window resize so whatever :/
+    window.set_app_paintable(True) #Needed for transparency ig
+
     GtkLayerShell.init_for_window(window)
-    GtkLayerShell.set_namespace(window, "Hyprmouse")
+    GtkLayerShell.set_namespace(window, "Hyprmouse") #Needed so you can apply layer styles via hyprland.conf to Hyprmouse
     GtkLayerShell.set_layer(window, GtkLayerShell.Layer.OVERLAY)
-    GtkLayerShell.set_keyboard_mode(window, GtkLayerShell.KeyboardMode.EXCLUSIVE)
-    window.connect('draw', draw_grid)
+    GtkLayerShell.set_keyboard_mode(window, GtkLayerShell.KeyboardMode.EXCLUSIVE) #Overlay now blocks the kb input
+
+    if config["show_ui"]:
+        if config["show_grid"]: window.connect("draw", draw_grid)
+        if config["show_numbers"]: window.connect("draw", draw_numbers)
     window.connect("key-press-event", on_key_press)
 
+    GLib.timeout_add(100, lambda: update(window))
+    window.connect("destroy", Gtk.main_quit)
     window.show_all()
-    window.connect('destroy', Gtk.main_quit)
     Gtk.main()
 
 
+"""
+PROGRAM
+"""
+
+
+config = {
+    "show_ui": True,
+    "show_grid": True,
+    "show_dots": True,
+    "show_numbers": True,
+    "format": "x, y",
+    "font": Gtk.Settings.get_default().get_property("gtk-font-name").rsplit(maxsplit=1)[0],
+    "grid_color": "#ECEFF4",
+    "dot_color": "#FFFFFF",
+    "text_color": "#ECEFF4",
+    "text_outline_color": "#2E3440",
+    "grid_thickness": 0.5,
+    "spacing": 200
+}
+
+
+startPosX, startPosY = map(int, subprocess.check_output(["hyprctl", "cursorpos"]).decode("utf-8").split(','))
+posX = startPosX
+posY = startPosY
 setproctitle.setproctitle("Hyprmouse")
+load_config()
+width, height = get_screen_res() #Setting values once on launch
 draw_window()
