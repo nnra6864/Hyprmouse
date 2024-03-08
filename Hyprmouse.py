15a11b735397a3faeded24060b15f45cc9738083
nnra6864 <enenragd@gmail.com>
2024-03-08 08:37:24 +0100

    Improved the looks quite a bit

--- Hyprmouse.py
+++ Hyprmouse.py
@@ -19,6 +19,11 @@ def get_screen_res():
     return map(int, wh.split('x'))
 
 
+def get_mouse_pos():
+    return map(int, subprocess.check_output(["hyprctl", "cursorpos"]).decode("utf-8").split(','))
+
+
+
 """
 FUNCTIONS
 """
@@ -48,17 +53,24 @@ def load_config():
         print("An error occurred while loading the config file:", e)
 
 
+def draw_background(window, cr):
+    cr.set_source_rgba(*hex_to_rgb(config["background_color"]), config["background_opacity"])
+    cr.rectangle(0, 0, width, height)
+    cr.fill()
+
+
 def draw_grid(window, cr):
     global config, width, height
     cr.set_source_rgb(*hex_to_rgb(config["grid_color"]))
     cr.set_line_width(config["grid_thickness"])
-
-    for x in range((width // 2) % config["spacing"], width, config["spacing"]):
+    
+    spacing = config["spacing"]
+    for x in range((width // 2) % spacing, width, spacing):
         cr.move_to(x, 0)
         cr.line_to(x, height)
         cr.stroke()
 
-    for y in range((height // 2) % config["spacing"], height, config["spacing"]):
+    for y in range((height // 2) % spacing, height, spacing):
         cr.move_to(0, y)
         cr.line_to(width, y)
         cr.stroke()
@@ -68,19 +80,47 @@ def draw_grid(window, cr):
 
 def draw_numbers(window, cr):
     global config, width, height, posX, posY
-    font = Gtk.Settings.get_default().get_property("gtk-font-name").rsplit(maxsplit=1)[0] #Don't ask o_0
-    cr.select_font_face(font, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    cr.set_font_size(20)
-
+    cr.select_font_face(config["font"], cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+    cr.set_font_size(config["font_size"])
+    spacing = config["spacing"]
+    
+    offset = config["text_outline_thickness"]
+    for x in range((width // 2) % spacing, width, spacing):
+        for y in range((height // 2) % spacing, height, spacing):
+            cr.set_source_rgb(*hex_to_rgb(config["text_outline_color"]))
+            label = f"{x - posX} {posY - y}"
+            xp = x - cr.text_extents(label)[4] // 2
+            yp = y + cr.text_extents(label)[3] // 2
+            cr.move_to(xp + offset, yp + offset)
+            cr.show_text(label)
+            cr.move_to(xp - offset, yp + offset)
+            cr.show_text(label)
+            cr.move_to(xp + offset, yp - offset)
+            cr.show_text(label)
+            cr.move_to(xp - offset, yp - offset)
+            cr.show_text(label)
+            
+            cr.set_source_rgb(*hex_to_rgb(config["text_color"]))
+            cr.move_to(xp, yp)
+            cr.show_text(label)
 
 
 def update(window):
+    global posX, posY
+    posX, posY = get_mouse_pos()
     window.queue_draw()
     return True
 
 
 def on_key_press(window, event):
     if event.keyval == Gdk.KEY_Delete:
+        Gtk.main_quit()
+
+    if event.keyval == Gdk.KEY_Escape:
+        subprocess.run(["ydotool", "mousemove", "-a", f"{startPosX}", f"{startPosY}"])
+        Gtk.main_quit()
+
+    if event.keyval == Gdk.KEY_Return:
         Gtk.main_quit()
 
 
@@ -98,11 +138,13 @@ def draw_window():
     GtkLayerShell.set_keyboard_mode(window, GtkLayerShell.KeyboardMode.EXCLUSIVE) #Overlay now blocks the kb input
 
     if config["show_ui"]:
+        if config["show_background"]: window.connect("draw", draw_background)
         if config["show_grid"]: window.connect("draw", draw_grid)
         if config["show_numbers"]: window.connect("draw", draw_numbers)
     window.connect("key-press-event", on_key_press)
 
-    GLib.timeout_add(100, lambda: update(window))
+    update_rate = int(1 / config["fps"] * 1000)
+    GLib.timeout_add(update_rate, lambda: update(window))
     window.connect("destroy", Gtk.main_quit)
     window.show_all()
     Gtk.main()
@@ -115,22 +157,26 @@ PROGRAM
 font_params = Gtk.Settings.get_default().get_property("gtk-font-name").split()
 config = {
     "show_ui": True,
+    "show_background": True,
     "show_grid": True,
     "show_dots": True,
     "show_numbers": True,
     "format": "x, y",
     "font": " ".join(font_params[:-1]),
     "font_size": int(font_params[-1]),
-    "grid_color": "#ECEFF4",
-    "dot_color": "#FFFFFF",
+    "background_color": "000000",
+    "grid_color": "#5e81ac",
+    "dot_color": "#ECEFF4",
     "text_color": "#ECEFF4",
     "text_outline_color": "#2E3440",
-    "grid_thickness": 0.5,
-    "spacing": 200
+    "fps": 30,
+    "text_outline_thickness": 1,
+    "background_opacity": 0.5,
+    "grid_thickness": 1,
+    "spacing": 400
 }
 
-
-startPosX, startPosY = map(int, subprocess.check_output(["hyprctl", "cursorpos"]).decode("utf-8").split(','))
+startPosX, startPosY = get_mouse_pos()
 posX = startPosX
 posY = startPosY
 setproctitle.setproctitle("Hyprmouse")
